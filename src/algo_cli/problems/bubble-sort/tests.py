217fc60dc164bb2815c8478fc4ffd2d19e66974f
lasse-cs <lasse.schmieding@gmail.com>
2026-04-07 15:38:24 +0100

    Use hypothesis

--- src/algo_cli/problems/bubble-sort/tests.py
+++ src/algo_cli/problems/bubble-sort/tests.py
@@ -1,15 +1,8 @@
+from hypothesis import given, strategies as st
+
 from solution import bubble_sort
 
 
-def test_empty_list():
-    assert [] == bubble_sort([])
-
-
-def test_single_item_list():
-    input = [5]
-    assert [5] == bubble_sort(input)
-
-
-def test_long_list():
-    input = [1, 5, 6, 7, 90, 11, 2131, 550, 4, 44, 67895, 10]
-    assert sorted(input) == bubble_sort(input)
+@given(st.lists(st.integers()))
+def test_matches_builtin(items):
+    assert sorted(items) == bubble_sort(items)
--- src/algo_cli/problems/insertion-sort/tests.py
+++ src/algo_cli/problems/insertion-sort/tests.py
@@ -1,15 +1,8 @@
+from hypothesis import given, strategies as st
+
 from solution import insertion_sort
 
 
-def test_empty_list():
-    assert [] == insertion_sort([])
-
-
-def test_single_item_list():
-    input = [5]
-    assert [5] == insertion_sort(input)
-
-
-def test_long_list():
-    input = [1, 5, 6, 7, 90, 11, 2131, 550, 4, 44, 67895, 10]
-    assert sorted(input) == insertion_sort(input)
+@given(st.lists(st.integers()))
+def test_matches_builtin(items):
+    assert sorted(items) == insertion_sort(items)
